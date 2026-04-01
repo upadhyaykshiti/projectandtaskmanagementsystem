c3357bee5d344a656c8d5049b36e7a8b2aa48d2f
Kshiti Upadhyay <kshiti.u@kiotel.co.in>
2026-04-01 18:58:58 +0530

    Updating index and not calling export routes when env is test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,11 +7,13 @@ import cors from "cors";
 import { authRouter } from "../src/routes/auth.routes";
 import { projectsRouter } from "../src/routes/project.routes";
 import { tasksRouter } from "../src/routes/task.routes";
-import { exportRouter } from "../src/routes/export.routes";
+// import { exportRouter } from "../src/routes/export.routes";
 import { errorHandler } from "../src/middleware/error";
+import { startExportWorker } from "./jobs/export.worker";
+
 
 const app = express();
-const PORT = process.env.PORT ?? 4000;
+const PORT = process.env.PORT ?? 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,8 +24,17 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/projects", projectsRouter);
 app.use("/api/tasks", tasksRouter);
-app.use("/api/export", exportRouter);
-app.use("/api/projects", exportRouter);
+// app.use("/api/export", exportRouter);
+// app.use("/api/projects", exportRouter);
+
+console.log("environment = ", process.env.NODE_ENV);
+if (process.env.NODE_ENV !== "test") {
+  const { exportRouter } = require("./routes/export.routes");
+  app.use("/api/exports", exportRouter);
+  app.use("/api/projects", exportRouter);
+
+}
+
 
 
 
@@ -39,8 +50,22 @@ app.use((req, res) => {
 });
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+ // Only start server + worker if NOT testing
+ 
+let server: any;
+
+if (process.env.NODE_ENV !== "test") {
+  server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+  startExportWorker();
+}
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 export default app;
+export { server };
